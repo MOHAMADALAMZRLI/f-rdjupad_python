a5bafab3039e8eb3b31d79f3747f96290e278be8
MOHAMADALAMZRLI <148483759+MOHAMADALAMZRLI@users.noreply.github.com>
2024-09-24 13:22:53 +0200

    Update read.py

--- read.py
+++ read.py
@@ -4,5 +4,5 @@ def read_data(file_path):
     """
     Läser in data från en CSV-fil med angiven filväg.
     """
-    df_1 = pd.read_csv(file_path, encoding='latin1')  # Läsa in CSV-filen med rätt encoding
+    df_1 = pd.read_csv(file_path, encoding='latin1')  
     return df_1
